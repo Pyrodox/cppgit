fb553e547a8320eedd124c95032cae59dd00ec2a
CPY <SirWilliamFarquadTheFourth@betterthanyou.com>
2021-05-05 16:14:13 -0700

    test

--- main.cpp
+++ main.cpp
@@ -3,6 +3,12 @@
 
 int main() 
 {
+    std::cout << "Please enter your first name: " << std::endl;
+    std::string name;
+    std::cin >> name;
+
+    const std::string greeting = "Hello, " + name + "!";
+
     const int pad = 1;
     const int rows = pad * 2 + 3;
 
@@ -12,6 +18,18 @@ int main()
     while (r != rows) {
         std::cout << std::endl;
         ++r;
+    }
+
+    const std::string::size_type cols = greeting.size() + pad * 2 + 2;
+    std::string::size_type c = 0;
+
+    while (c != cols) {
+        if (r == 0 || r == rows - 1 || c == 0 || c == cols - 1){
+            std::cout << "*";
+        } else {
+            std::cout << c << std::endl;
+            ++c;
+        }
     }
 
     return 0;
