ae0bacf9c9d1c0519c2425343822b8a33e4d5a3c
CPY <SirWilliamFarquadTheFourth@betterthanyou.com>
2021-05-05 16:39:25 -0700

    commit

--- main.cpp
+++ main.cpp
@@ -1,34 +1,37 @@
 #include <iostream>
 #include <string>
+using std::cout; using std::endl; using std::cin; using std::string;
 
 int main() 
 {
-    std::cout << "Please enter your first name: " << std::endl;
-    std::string name;
-    std::cin >> name;
+    string name;
 
-    const std::string greeting = "Hello, " + name + "!";
+    cout << "Please enter your first name: " << endl;
+    cin >> name;
+
+    const string greeting = "Hello, " + name + "!";
 
     const int pad = 1;
     const int rows = pad * 2 + 3;
+    const string::size_type cols = greeting.size() + pad * 2 + 2;
 
-    std::cout << std::endl;
-    int r = 0;
+    cout << endl;
 
-    while (r != rows) {
-        std::cout << std::endl;
-        ++r;
-    }
+    for(int r = 0; r != rows; ++r) {
+        string::size_type c = 0;
 
-    const std::string::size_type cols = greeting.size() + pad * 2 + 2;
-    std::string::size_type c = 0;
-
-    while (c != cols) {
-        if (r == 0 || r == rows - 1 || c == 0 || c == cols - 1){
-            std::cout << "*";
-        } else {
-            std::cout << c << std::endl;
-            ++c;
+        while (c != cols) {
+            if (r == pad + 1 && c == pad + 1) {
+                    cout << greeting;
+                    c += greeting.size(); 
+            }
+            else {
+                if(r == 0 || r == rows - 1 || c == 0 || c == cols - 1) {
+                    cout << "*"; }
+                else 
+                    cout << " ";
+                    ++c;
+            }
         }
     }
 
