e2824e87a9064e34b75c1b14a11af356f2c3db44
CPY <SirWilliamFarquadTheFourth@betterthanyou.com>
2021-05-05 17:39:29 -0700

    exercise 3?

--- main.cpp
+++ main.cpp
@@ -6,14 +6,19 @@ int main()
 {
     string name;
 
-    cout << "Please enter your first name: " << endl;
+    cout << "Please enter your first name: ";
     cin >> name;
 
     const string greeting = "Hello, " + name + "!";
 
-    const int pad = 1;
+    string::size_type pad;
+    string::size_type pad_sides;
+    cout << "Please enter the amount of space between the top and bottom of the greeting you want: ";
+    cin >> pad;
+    cout << "Please enter the amount of space between the sides of the greeting you want: ";
+    cin >> pad_sides;
     const int rows = pad * 2 + 3;
-    const string::size_type cols = greeting.size() + pad * 2 + 2;
+    const string::size_type cols = greeting.size() + pad_sides * 2 + 2;
 
     cout << endl;
 
@@ -21,18 +26,19 @@ int main()
         string::size_type c = 0;
 
         while (c != cols) {
-            if (r == pad + 1 && c == pad + 1) {
+            if (r == pad + 1 && c == pad_sides + 1) {
                     cout << greeting;
                     c += greeting.size(); 
             }
             else {
-                if(r == 0 || r == rows - 1 || c == 0 || c == cols - 1) {
-                    cout << "*"; }
-                else 
-                    cout << " ";
-                    ++c;
+                    if(r == 0 || r == rows - 1 || c == 0 || c == cols - 1)
+                        cout << "*";
+                    else
+                        cout << " ";
+                        ++c;
             }
         }
+        cout << endl;
     }
 
     return 0;
