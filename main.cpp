7441032700bc2d652969ecdc790a4fef91485d14
CPY <SirWilliamFarquadTheFourth@betterthanyou.com>
2021-05-06 17:16:29 -0700

    calculate grades program

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <ios>
+using std::cout; using std::string; using std::endl; using std::cin; using std::setprecision; using std::streamsize;
+
+int main(){
+    cout << "Please enter your first name: ";
+    string name;
+    cin >> name;
+    cout << "Hello, " + name + "!" << endl;
+
+    cout << "Please enter your midterm and final exam grades: ";
+    double midterm, final;
+    cin >> midterm >> final;
+
+    cout << "Enter all your homework grades, "
+         "followed by end-of-file: ";
+
+    int count = 0;
+    double sum = 0;
+    double x;
+
+    while (cin >> x) {
+        ++count;
+        sum += x;
+    }
+
+    streamsize prec = cout.precision();
+    cout << "Your final grade is " << setprecision(3)
+    << 0.2 * midterm + 0.4 * final + 0.4 * sum / count
+    << setprecision(prec) << endl;
+    
+    return 0;
+}
