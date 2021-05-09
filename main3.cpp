1923a22ae45f0ee1e6b7be27e674b2d58bf04f51
CPY <SirWilliamFarquadTheFourth@betterthanyou.com>
2021-05-09 14:58:45 -0700

    3-6

---
+++ main3.cpp
@@ -0,0 +1,43 @@
+#include <iomanip>
+#include <ios>
+#include <iostream>
+#include <string>
+using std::cin; using std::setprecision; using std::cout; 
+using std::string; using std::endl; using std::streamsize;
+
+int main() {
+    double midterm, final;  
+    string name;
+    int count = 0;    
+    double sum = 0;  
+    double x;
+    streamsize prec = cout.precision();
+
+    cout << "Please enter your first name: ";        
+    cin >> name;    
+    cout << "Hello, " << name << "!" << endl;
+    
+    cout << "Please enter your midterm and final exam grades: ";      
+    cin >> midterm >> final;    
+    
+    cout << "Enter all your homework grades, "         
+    "followed by end-of-file: ";
+    while (cin >> x) {        
+        ++count;        
+        sum += x;    
+    
+    }
+    double average = sum / count;
+    setprecision(3);
+
+    if (count > 0) {    
+        cout << "Your final grade is " << 0.2 * midterm + 0.4 * final + average << endl; 
+    }
+    else {    
+        cout << "no homework entered " << endl;
+    }
+
+    setprecision(prec);
+     
+    return 0;
+}
