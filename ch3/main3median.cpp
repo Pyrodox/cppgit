5ad5c1cf2423550dbbbad244dafada03d621dc53
CPY <SirWilliamFarquadTheFourth@betterthanyou.com>
2021-05-21 17:51:59 -0700

    fix

--- ch3/main3median.cpp
+++ ch3/main3median.cpp
@@ -37,16 +37,15 @@ int main()
             cin >> x;
             homework.push_back(x);
         }
-
-        typedef vector<double>::size_type vec_sz;    
-        vec_sz size = homework.size();    
+   
+        int size = homework.size();
+        int mid = size / 2;    
         if (size == 0) {        
             cout << endl << "You must enter your grades.  "                        
             "Please try again." << endl;         
             return 1;    
         }    
-        sort(homework.begin(), homework.end());   
-        vec_sz mid = size / 2;
+        sort(homework.begin(), homework.end());
             
         double median;    
         median = size % 2 == 0 ? (homework[mid] + homework[mid-1]) / 2 : homework[mid];
