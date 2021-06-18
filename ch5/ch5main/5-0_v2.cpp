265ab46fbedfc1185f5a742372a97bbdfefcb408
CPY <SirWilliamFarquadTheFourth@betterthanyou.com>
2021-06-17 18:18:54 -0700

    5-6

--- ch5/ch5main/5-0_v2.cpp
+++ ch5/ch5main/5-0_v2.cpp
@@ -5,31 +5,38 @@
 #include <iomanip>
 #include <stdexcept>
 #include <ios>
+#include <chrono>
 #include "grade.h"
 #include "Student_info.h"
-using std::cout; using std::vector; using std::endl; using std::max; using std::string;
+using std::cout; using std::vector; using std::endl; using std::max; using std::string; using std::milli;
 using std::sort; using std::streamsize; using std::setprecision; using std::domain_error; using std::cin;
-
+//pre 5-6 main change (using resize instead of erase): 382.951 ms, post 5-6 main change (using resize instead of erase): 258.873 ms
 bool fgrade(Student_info& s)
 {    
     return grade(s) < 60;
 }
 
-vector<Student_info> extract_fails(vector<Student_info>& students)
+vector<Student_info> extract_fails(vector<Student_info>& v)
 {    
-    vector<Student_info> fail;    
-    vector<Student_info>::size_type i = 0;    
+    vector<Student_info> fail, vcopy;    
+    vector<Student_info>::size_type i = 0;
+    int newvecsz = 0;
     
     // invariant:elements [0, i) of students represent passing grades     
-    while (i != students.size()) {         
-        if (fgrade(students[i])) {
-            fail.push_back(students[i]);            
-            students.erase(students.begin() + i);        
-        } 
+    while (i != v.size()) {         
+        if (fgrade(v[i])) {
+            fail.push_back(v[i]);
+        }
         else {
-            ++i;
-        }    
-    }    
+            newvecsz++;
+            vcopy.push_back(v[i]);
+        }
+
+        i++;
+    }
+
+    v.insert(v.begin(), vcopy.begin(), vcopy.begin() + newvecsz);
+    v.resize(newvecsz);
     
     return fail;
 }
@@ -41,6 +48,9 @@ int main()
     string::size_type maxlen = 0;
 
     cout << "Enter name, midterm grade, finals grade, and homework grades." << endl;
+
+    auto start = std::chrono::steady_clock::now();
+
     while (read(cin, record)) {
         maxlen = max(maxlen, record.name.size());
         students.push_back(record);
@@ -80,6 +90,11 @@ int main()
         }
         cout << endl;
     }
+
+    auto end = std::chrono::steady_clock::now();
+    auto diff = end - start;
+
+    cout << std::chrono::duration <double, milli> (diff).count() << " ms";
 
     return 0;
 }
